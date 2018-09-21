74b4840fe6b471cae4ce7c14be24129185437976
Dmitry Krokhin <dmitry.krokhin@basis.company>
2018-09-21 21:35:55 +0300

    fix schema management close #30

--- php/Job/Space/AddProperty.php
+++ php/Job/Space/AddProperty.php
@@ -14,10 +14,12 @@ class AddProperty extends Job
     {
         $space = $this->getSpace();
 
-        if($space->hasProperty($this->name)) {
+        if ($space->hasProperty($this->name)) {
             throw new Exception("Property $this->name exists");
         }
 
-        $space->addProperty($this->name, $this->type, $this->is_nullable);
+        $space->addProperty($this->name, $this->type, [
+            'is_nullable' => $this->is_nullable
+        ]);
     }
 }
