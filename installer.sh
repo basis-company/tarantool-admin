2583d878eda803e396c75512b61519d19a938062
Andrey Milovanov <Tremorus@users.noreply.github.com>
2019-01-16 11:47:12 +0300

    Fix installer (#31)
    libraries installer fix, tune git ignore

--- installer.sh
+++ installer.sh
@@ -1,14 +1,14 @@
-mkdir client-libraries
+mkdir ./public/admin
 
-wget https://use.fontawesome.com/releases/v5.0.6/fontawesome-free-5.0.6.zip
-unzip fontawesome-free-5.0.6.zip
-mv fontawesome-free-5.0.6/on-server client-libraries/fontawesome-free-5.0.6
-rm -rf ./fontawesomels -free-5.0.6*
+wget https://use.fontawesome.com/releases/v5.0.6/fontawesome-free-5.0.6.zip .
+unzip ./fontawesome-free-5.0.6.zip
+mv fontawesome-free-5.0.6/on-server ./public/admin/fontawesome-free-5.0.6
+rm -rf ./fontawesome-free-5.0.6*
 
-wget http://cdn.sencha.com/ext/gpl/ext-6.2.0-gpl.zip
-unzip ext-6.2.0-gpl.zip
-mkdir client-libraries/ext-6.2.0
-mv ext-6.2.0/build/ext-all.js client-libraries/ext-6.2.0/ext-all.js
-mkdir client-libraries/ext-6.2.0/classic
-mv ext-6.2.0/build/classic/theme-crisp client-libraries/ext-6.2.0/classic/theme-crisp
+wget http://cdn.sencha.com/ext/gpl/ext-6.2.0-gpl.zip .
+unzip ./ext-6.2.0-gpl.zip
+mkdir ./public/admin/ext-6.2.0
+mv ext-6.2.0/build/ext-all.js ./public/admin/ext-6.2.0/ext-all.js
+mkdir ./public/admin/ext-6.2.0/classic
+mv ext-6.2.0/build/classic/theme-crisp ./public/admin/ext-6.2.0/classic/theme-crisp
 rm -rf ./ext-6.2.0*
