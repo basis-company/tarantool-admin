6fe1c708902d493fd6cc1fb4bbed2f6521faad58
Dmitry Krokhin <nekufa@gmail.com>
2020-06-25 14:50:23 +0300

    fix space instance name

--- php/Job/Space/Job.php
+++ php/Job/Space/Job.php
@@ -7,15 +7,15 @@ use Job\Database\Job as DatabaseJob;
 
 abstract class Job extends DatabaseJob
 {
-    private $space;
+    private $spaceInstance;
     public function getSpace()
     {
-        if (!$this->space) {
+        if (!$this->spaceInstance) {
             if (!$this->space) {
                 throw new Exception("Invalid params");
             }
-            $this->space = $this->getMapper()->getSchema()->getSpace($this->space);
+            $this->spaceInstance = $this->getMapper()->getSchema()->getSpace($this->space);
         }
-        return $this->space;
+        return $this->spaceInstance;
     }
 }
