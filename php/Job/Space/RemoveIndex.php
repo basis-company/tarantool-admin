6e8db0ef3e61b4e930b802f02fccd42e127f9e6f
Firetawnyowl <oktan9@yandex.ru>
2024-05-17 16:12:57 +0300

    query refactor

--- php/Job/Space/RemoveIndex.php
+++ php/Job/Space/RemoveIndex.php
@@ -11,11 +11,10 @@ class RemoveIndex extends Job
     public function run(): void
     {
         $space = $this->getSpace();
-        $spaceName = $space->getName();
         $indexExist = false;
         foreach ($space->mapper->find('_vindex', ['id' => $space->getId()]) as $index) {
             if ($index['name'] == $this->name) {
-                $space->mapper->client->call("box.space.$spaceName.index.$this->name:drop");
+                $space->mapper->client->call("box.space." . $space->getName() . ".index.$this->name:drop");
                 $indexExist = true;
             }
         }
